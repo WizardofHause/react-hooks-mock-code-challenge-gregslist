55232ed28da1f9a7075d97a3082c11b861f4d935
Jon Hause <jonrhause@gmail.com>
2022-10-18 13:37:41 -0600

    commit tree 82733e1dc44265e42d4dd8ca413d8abf5b326216

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  const [search, setSearch] = useState('')
+
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value)
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header search={search} handleSearch={handleSearch} />
+      <ListingsContainer search={search} handleSearch={handleSearch} />
     </div>
   );
 }
 
 export default App;
+
+// import React, { useState, useEffect } from "react";
+// import Header from "./Header";
+// import ListingsContainer from "./ListingsContainer";
+
+// function App() {
+
+//   const [listings, setListings] = useState([])
+//   const [search, setSearch] = useState('')
+
+// useEffect(() => {
+//   fetch("http://localhost:6001/listings")
+//   .then(response => response.json())
+//   .then((listings) => setListings(listings))
+// }, []);
+
+// const handleSearch = (e) => {
+//   setSearch(e.target.value)
+// }
+
+//   return (
+//     <div className="app">
+//       <Header search={search} handleSearch={handleSearch}/>
+//       <ListingsContainer listingData={listings} search={search} handleSearch={handleSearch}/>
+//     </div>
+//   );
+// }
+
+// export default App;
