b8ecac09e53ed3a6d86f6a85e9c79baf2b07f40e
Jon Hause <jonrhause@gmail.com>
2022-10-18 14:09:21 -0600

    stars, delete, rearrange stuff

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,24 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+function ListingCard({ listing, onDeleteListing }) {
+
+  const { id, description, image, location } = listing
+
+  const [isFavorite, setIsFavorite] = useState(false)
+
+  const handleDelete = () => {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+    .then(() => {
+      onDeleteListing(id)
+    })
+  }
+
+  const handleFavorite = () => {
+    setIsFavorite(!isFavorite)
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {isFavorite ? (
+          <button onClick={handleFavorite} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={handleFavorite} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button className="emoji-button delete" onClick={handleDelete}>🗑</button>
       </div>
     </li>
   );
 }
 
 export default ListingCard;
+
+
+// import React, {useState} from "react";
+
+// function ListingCard({description, image, location, handleDelete}) {
+
+//   const [isFavorite, setIsFavorite] = useState(false)
+
+//   const handleFavorite = () => {
+//     setIsFavorite(!isFavorite)
+//   }
+
+//   return (
+//     <li className="card">
+//       <div className="image">
+//         <span className="price">$0</span>
+//         <img src={image} alt={description} />
+//       </div>
+//       <div className="details">
+//         {isFavorite ? (
+//           <button onClick={handleFavorite} className="emoji-button favorite active">★</button>
+//         ) : (
+//           <button onClick={handleFavorite} className="emoji-button favorite">☆</button>
+//         )}
+//         <strong>{description}</strong>
+//         <span> · {location}</span>
+//         <button className="emoji-button delete" onClick={handleDelete}>🗑</button>
+//       </div>
+//     </li>
+//   );
+// }
+
+// export default ListingCard;
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,67 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+
+function ListingsContainer({ search }) {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(response => response.json())
+      .then((listings) => setListings(listings))
+  }, []);
+
+  const handleDeleteListing = (id) => {
+    const newListingArray = listings.filter(listing => listing.id !== id)
+    setListings(newListingArray)
+  }
+
+  const filteredListings = listings.filter(listing => listing.description.includes(search))
+
+  const listingCard = filteredListings.map((listing) => {
+    return <ListingCard
+      key={listing.id}
+      listing={listing}
+      onDeleteListing={handleDeleteListing}
+    />
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCard}
       </ul>
     </main>
   );
 }
 
 export default ListingsContainer;
+
+
+// import React from "react";
+// import ListingCard from "./ListingCard";
+
+// function ListingsContainer({listingData, search}) {
+
+// const filteredListings = listingData.filter(listing => listing.description.includes(search))
+
+// const listingCard = filteredListings.map((listing) => {
+//   return <ListingCard 
+//     key={listing.id} 
+//     description={listing.description} 
+//     image={listing.image} 
+//     location={listing.location}
+//     />
+// })
+
+//   return (
+//     <main>
+//       <ul className="cards">
+//         {listingCard}
+//       </ul>
+//     </main>
+//   );
+// }
+
+// export default ListingsContainer;
